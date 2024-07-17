5f046e62f6e9e0fab56fb285446861b8e53d1a9b
seonyo <jsy6535@naver.com>
2024-07-17 11:44:40 +0900

    layout 익히기

---
+++ app/cart/page.tsx
@@ -0,0 +1,7 @@
+export default function Cart() {
+  return(
+    <div>
+        <h1>장바구니 입니다</h1>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link"
-
 export default function Home() {
   let name :string = 'sunny'
   let link :string = 'http://google.com'
